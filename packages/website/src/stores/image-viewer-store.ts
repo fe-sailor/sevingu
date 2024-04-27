3983518196f6d7b613c3d296592827cf64b408a4
obov <obov3e@gmail.com>
2024-04-27 18:12:40 +0900

    feat: show uploaded image

---
+++ packages/website/src/stores/image-viewer-store.ts
@@ -0,0 +1,77 @@
+import { create } from 'zustand';
+
+export const ImageViewerState = {
+	Default: 'Default',
+	ElementLoaded: 'ElementLoaded',
+	ImageLoaded: 'ImageLoaded',
+} as const;
+
+interface ImageViewerStore {
+	imageViewer: null | HTMLCanvasElement;
+	setImageViewer: (imageViewer: HTMLCanvasElement) => void;
+	setImageSrc: (imageUrl: string) => void;
+	getImageUri: (fileChangeEvent: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const useImageViewerStore = create<ImageViewerStore>((set, get) => ({
+	imageViewer: null,
+	setImageViewer: (imageViewer: HTMLCanvasElement) =>
+		set(() => ({ imageViewer })),
+	setImageSrc: (imageUrl: string) => {
+		const imageViewer = get().imageViewer;
+		if (!imageViewer) {
+			return;
+		}
+		imageViewer.onload = () => {};
+		imageViewer.onerror = () => {};
+	},
+	getImageUri: (fileChangeEvent: React.ChangeEvent<HTMLInputElement>) => {
+		if (!fileChangeEvent.target.files?.length) {
+			return;
+		}
+
+		const reader = new FileReader();
+		reader.onloadend = () => {
+			const imageViewer = get().imageViewer;
+
+			if (!imageViewer) {
+				console.error('image view empty');
+				return;
+			}
+
+			if (typeof reader.result !== 'string') {
+				console.error('image data uri is not string');
+				return;
+			}
+
+			renderOnViewer(reader.result, imageViewer);
+		};
+
+		reader.onerror = () => {};
+
+		reader.readAsDataURL(fileChangeEvent.target.files[0]);
+	},
+}));
+
+const renderOnViewer = (imageUri: string, canvasRef: HTMLCanvasElement) => {
+	const htmlRenderedImage = new Image();
+
+	htmlRenderedImage.onload = () => {
+		const height = (canvasRef.height = htmlRenderedImage.height);
+		const width = (canvasRef.width = htmlRenderedImage.width);
+
+		const canvas2dContext = canvasRef.getContext('2d');
+		if (!canvas2dContext) {
+			console.error('canvas context empty');
+			return;
+		}
+		canvas2dContext.drawImage(htmlRenderedImage, 0, 0, width, height);
+
+		canvas2dContext.getImageData(0, 0, width, height);
+
+		// TODO: image config 반영
+		// ctx.putImageData(this.imageData, 0, 0);
+	};
+
+	htmlRenderedImage.src = imageUri;
+};
