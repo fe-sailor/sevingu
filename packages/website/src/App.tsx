73714f3e13705b89ebb87764c21bb6f7203af833
MC Kim <jkl851@naver.com>
2024-04-27 18:16:27 +0900

    feat: add states of store

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,10 +1,60 @@
+import { useRef } from 'react';
+import { Button } from '@/components/ui/button';
 import MainViewer from './components/main-viewer/MainViewer';
+import { useStore } from '@/stores/store';
 
 function App() {
+	const [, setCurImage, undo, redo] = useStore(state => [
+		state.curImage,
+		state.setCurImage,
+		state.undo,
+		state.redo,
+	]);
+	const fileInputRef = useRef<HTMLInputElement>(null);
+
+	const handleUploadButtonClick = () => {
+		if (fileInputRef.current) {
+			fileInputRef.current.click();
+		}
+	};
+
+	const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const file = e.target.files?.[0];
+		const reader = new FileReader();
+
+		reader.onloadend = () => {
+			setCurImage(reader.result as string);
+		};
+
+		if (file) {
+			reader.readAsDataURL(file);
+		}
+	};
+
 	return (
-		<div className="px-10">
-			<MainViewer />
-		</div>
+		<>
+			<div className="px-10">
+				<MainViewer />
+			</div>
+			<div className="flex gap-4 m-4">
+				<Button variant="destructive" onClick={handleUploadButtonClick}>
+					Image Upload
+				</Button>
+				<Button variant="destructive" onClick={undo}>
+					undo
+				</Button>
+				<Button variant="destructive" onClick={redo}>
+					redo
+				</Button>
+			</div>
+			<input
+				type="file"
+				ref={fileInputRef}
+				style={{ display: 'none' }}
+				onChange={handleFileChange}
+			/>
+			<Panel />
+		</>
 	);
 }
 
--- packages/website/src/stores/store.ts
+++ packages/website/src/stores/store.ts
@@ -1,5 +1,63 @@
 import { create } from 'zustand';
+import logo from '@/assets/sevingu_logo.png';
 
-interface SevinguState {}
+interface SevinguState {
+	curImage: string;
+	pastImages: string[];
+	futureImages: string[];
+	setCurImage: (img: string) => void;
+	undo: () => void;
+	redo: () => void;
+}
 
-export const useStore = create<SevinguState>(() => ({}));
+export const useStore = create<SevinguState>(set => ({
+	curImage: logo,
+	pastImages: [],
+	futureImages: [],
+	setCurImage: img =>
+		set(state => {
+			const newPastImages = state.curImage
+				? [...state.pastImages, state.curImage]
+				: [...state.pastImages];
+			return {
+				...state,
+				curImage: img,
+				pastImages: newPastImages,
+				futureImages: [],
+			};
+		}),
+	undo: () =>
+		set(state => {
+			if (state.pastImages.length === 0) return state;
+
+			const prevImage = state.pastImages[state.pastImages.length - 1];
+			const newPastImages = state.pastImages.slice(0, -1);
+			const newFutureImages = state.curImage
+				? [state.curImage, ...state.futureImages]
+				: [...state.futureImages];
+
+			return {
+				...state,
+				curImage: prevImage,
+				pastImages: newPastImages,
+				futureImages: newFutureImages,
+			};
+		}),
+	redo: () =>
+		set(state => {
+			if (state.futureImages.length === 0) return state;
+
+			const nextImage = state.futureImages[0];
+			const newFutureImages = state.futureImages.slice(1);
+			const newPastImages = state.curImage
+				? [...state.pastImages, state.curImage]
+				: [...state.pastImages];
+
+			return {
+				...state,
+				curImage: nextImage,
+				pastImages: newPastImages,
+				futureImages: newFutureImages,
+			};
+		}),
+}));
