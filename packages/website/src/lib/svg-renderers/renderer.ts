1d7b30c7328d3299a09aafdb99cfbdb366cb9ddc
obov <obov3e@gmail.com>
2024-05-01 20:02:01 +0900

    feat: set setting on renderer

--- packages/website/src/lib/svg-renderers/renderer.ts
+++ packages/website/src/lib/svg-renderers/renderer.ts
@@ -1,7 +1,13 @@
 import { RenderSvg } from '@/lib/svg-renderers/bases';
 import { CircleService } from '@/lib/svg-renderers/circle';
-import { SvgRendererSetting } from '@/lib/svg-renderers/renderer-schema';
-import { stringJoin, exhaustiveTypeCheck } from '@/lib/utils';
+import {
+	SvgRendererSetting,
+	SvgSetting,
+	svgRendererSettingSchema,
+} from '@/lib/svg-renderers/renderer-schema';
+import { exhaustiveTypeCheck, stringJoin } from '@/lib/utils';
+import { SvgSettingSvgurt } from '@/stores/store';
+import _ from 'lodash';
 
 export class SvgRenderService {
 	private static setting: SvgRendererSetting;
@@ -24,8 +30,9 @@ export class SvgRenderService {
 		this.height = height;
 	}
 
-	static setSetting(setting: SvgRendererSetting) {
-		this.setting = setting;
+	static setSetting(setting: SvgSettingSvgurt | SvgSetting) {
+		this.setting = this.adaptSetting(setting);
+		this.setSubSetting(setting);
 	}
 
 	private static getRenderer(): typeof RenderSvg {
@@ -38,7 +45,36 @@ export class SvgRenderService {
 		}
 	}
 
+	private static setSubSetting(setting: SvgSettingSvgurt | SvgSetting) {
+		const { renderType } = this.setting;
+		switch (renderType) {
+			case 'CIRCLE':
+				CircleService.setCircleSetting(setting);
+				return;
+			default:
+				throw exhaustiveTypeCheck(renderType);
+		}
+	}
+
 	private static getSizeString(): string {
 		return `height="${this.height * this.setting.scale}" width="${this.width * this.setting.scale}"`;
+	}
+
+	private static adaptSetting(
+		setting: SvgSettingSvgurt | SvgSetting
+	): SvgRendererSetting {
+		if (this.validateSvgSetting(setting)) {
+			return _.pick(setting, ['renderType', 'scale']);
+		}
+		return {
+			renderType: setting.svgRenderType,
+			scale: setting.scale,
+		};
+	}
+
+	private static validateSvgSetting(
+		setting: SvgSettingSvgurt | SvgSetting
+	): setting is SvgSetting {
+		return svgRendererSettingSchema.safeParse(setting).success;
 	}
 }
