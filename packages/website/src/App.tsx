8bc8c35d727b1502e97e5e54618903401eeef73f
MC Kim <jkl851@naver.com>
2024-05-11 16:14:39 +0900

    fix: add download button

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -7,18 +7,6 @@ import DualProcessedImageViewer from './components/dual-processed-image-viewer/D
 import { useMessageListener } from './stores/messageStore';
 
 function App() {
-	const [originalDownload] = useStore(state => [state.download]);
-
-	// 광고를 표시하고 5초 후에 다운로드를 실행
-	const downloadWithAd = useCallback(() => {
-		console.log('광고 표시 시작...');
-
-		setTimeout(() => {
-			console.log('5초 지남, 다운로드 시작...');
-			originalDownload();
-		}, 5000); // 5초 대기
-	}, [originalDownload]);
-
 	useMessageListener({
 		on: 'Any',
 		listener: state => {
--- packages/website/src/components/header/MainHeader.tsx
+++ packages/website/src/components/header/MainHeader.tsx
@@ -3,9 +3,11 @@ import logo from '@/assets/sevingu_logo.png';
 import { useStore } from '@/stores/store';
 import { useMessageListener } from '@/stores/messageStore';
 import { Input } from '../ui/input';
+import { useCallback } from 'react';
 
 const MainHeader = () => {
-	const { showImage, undo, redo } = useStore();
+	const { showImage, undo, redo, download } = useStore();
+
 	const handleImageChange: React.ChangeEventHandler<
 		HTMLInputElement
 	> = event => {
@@ -14,6 +16,16 @@ const MainHeader = () => {
 		}
 		showImage(event.target.files[0]);
 	};
+
+	// 광고를 표시하고 5초 후에 다운로드를 실행
+	const downloadWithAd = useCallback(() => {
+		console.log('광고 표시 시작...');
+
+		setTimeout(() => {
+			console.log('5초 지남, 다운로드 시작...');
+			download();
+		}, 5000); // 5초 대기
+	}, [download]);
 
 	return (
 		<div className={'flex gap-9'}>
@@ -29,6 +41,11 @@ const MainHeader = () => {
 			</Button>
 			<Button variant="destructive" onClick={redo}>
 				redo
+			</Button>
+
+			{/* downloadWithAd 함수를 사용하도록 수정 */}
+			<Button variant="destructive" onClick={downloadWithAd}>
+				download
 			</Button>
 		</div>
 	);
