961cef6ee6f728761d94af5eb3b096f585862cad
obov <obov3e@gmail.com>
2024-05-12 15:06:35 +0900

    feat: add dragable componet for panel

--- packages/website/src/App.tsx
+++ packages/website/src/App.tsx
@@ -1,11 +1,10 @@
-import { useStore } from '@/stores/store';
-import { useCallback } from 'react'; // useCallback 추가
+import { isProd } from '@/lib/utils';
 import DualProcessedImageViewer from './components/dual-processed-image-viewer/DualProcessedImageViewer';
 import MainHeader from './components/header/MainHeader';
 import MainPanel from './components/panel/MainPanel';
 import { cn } from './lib/utils';
 import { useMessageListener } from './stores/messageStore';
-import { isProd } from '@/lib/utils';
+import { Rnd } from 'react-rnd';
 
 function App() {
 	useMessageListener({
@@ -19,13 +18,23 @@ function App() {
 			console.groupEnd();
 		},
 	});
+	console.log('Rnd', <Rnd />);
 
 	return (
 		<>
 			<MainHeader />
 			<div className={cn('flex')}>
 				<DualProcessedImageViewer />
-				<MainPanel />
+				{/* ref: https://github.com/bokuweb/react-rnd */}
+				<Rnd
+					default={{
+						x: 0,
+						y: 0,
+						width: 320,
+						height: 200,
+					}}>
+					<MainPanel />
+				</Rnd>
 			</div>
 		</>
 	);
