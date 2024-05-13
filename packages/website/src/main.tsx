264e4f48f1fdaf079864f054d5b74f4b4d27cc39
obov <obov3e@gmail.com>
2024-05-14 00:18:12 +0900

    feat: turn off console log on prod

--- packages/website/src/main.tsx
+++ packages/website/src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import '../app/globals.css';
+import { isProd } from './lib/utils.ts';
+
+if (isProd()) {
+	console.log = () => {};
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
